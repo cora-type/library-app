4c7b3b48197954e94ba7ca3f73e10df1b2e5546d
verloxs <gtsbyxx@gmail.com>
2022-04-18 20:53:28 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return title + " by " + author + ", " + pages + " pages," + read;
+  };
+}
+
+let example = new Book("Circe", "Madeline Miller", 393, false);
