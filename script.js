aba99e4baf1e1ebf2d50ec870a4b14339a77d707
verloxs <gtsbyxx@gmail.com>
2022-04-21 20:17:35 -0400

    added delete functionality

--- script.js
+++ script.js
@@ -11,12 +11,7 @@ function Book(title, author, description, read) {
   this.author = author;
   this.description = description;
   this.read = read;
-  // this.info = function () {
-  //   return title + " by " + author + ", " + pages + " pages," + read;
-  // };
 }
-
-const parent = document.querySelectorAll(".card");
 
 // result is the Object, id is the number
 let run = () =>
@@ -26,26 +21,13 @@ let run = () =>
     if (!myEle) {
       let content = `
   <div class="card" id="${idx}">
+  <button class="delete-button" onclick="deleter(this)" id="${idx}"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg></button>
     <div class="card-title">${result.title}</div>
     <div class="card-author">${result.author}</div>
               <div class="description">${result.description}</div>
     <div class="read-status">
       <button id="read-button">
-        ${result.read}<svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-check-circle"
-            >
-              <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-              <polyline points="22 4 12 14.01 9 11.01"></polyline>
-            </svg>
+        ${result.read}
           </button>
         </div>
         </div>
@@ -55,10 +37,33 @@ let run = () =>
     }
   });
 
+let run2 = () =>
+  myLibrary.forEach((result, idx) => {
+    let content = `
+  <div class="card" id="${idx}">
+  <button class="delete-button" onclick="deleter(this)" id="${idx}"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg></button>
+    <div class="card-title">${result.title}</div>
+    <div class="card-author">${result.author}</div>
+              <div class="description">${result.description}</div>
+    <div class="read-status">
+      <button id="read-button">
+        ${result.read}
+          </button>
+        </div>
+        </div>
+  `;
+
+    cardContainer.innerHTML += content;
+  });
+
+let reset = () => {
+  cardContainer.innerHTML = "";
+};
+
 //buttons
 const addBookBtn = document.getElementById("addBookBtn");
 const submit = document.getElementById("submit");
-const deleteButton = document.getElementById("delete");
+const deleteButton = document.querySelector(".delete-button");
 
 //containers
 const modal_container = document.getElementById("modal_container");
@@ -76,8 +81,6 @@ addBookBtn.addEventListener("click", () => {
   modal_container.classList.add("show");
   body.style.cssText = "filter: blur(3px);";
 });
-
-// deleteButton.addEventListener("click", () => {});
 
 submit.addEventListener("click", (event) => {
   event.preventDefault();
@@ -97,3 +100,9 @@ submit.addEventListener("click", (event) => {
 window.addEventListener("load", (event) => {
   run();
 });
+
+let deleter = (element) => {
+  reset();
+  myLibrary.splice(element.id, 1);
+  run2();
+};
