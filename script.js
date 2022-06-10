702293235ac3aafe2c2fc20bb98a048f4a35a00b
shy realm <gtsbyxx@gmail.com>
2022-06-10 02:51:15 -0400

    dont know...

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 let cardContainer = document.querySelector(".card-container");
-let a = new Book("1984", "George Orwell", "Dystopian O.o", "read");
-let myLibrary = [a];
+let readButton;
 
 function addBookToLibrary(x) {
   myLibrary.push(x);
 }
 
-function Book(title, author, description, read) {
-  this.title = title;
-  this.author = author;
-  this.description = description;
-  this.read = read;
+class Book {
+  constructor(title, author, description, read) {
+    this.title = title;
+    this.author = author;
+    this.description = description;
+    this.read = read;
+  }
+
+  set readStatus(x) {
+    this.read = x;
+  }
 }
+
+let a = new Book("1984", "George Orwell", "Dystopian O.o", "read");
+let myLibrary = [a];
 
 // adds cards to display, need to redo this function, too complicated and unscalable.
 // result is the Object, id is the number
@@ -21,17 +29,17 @@ let run = () =>
     // If a card with the current object doesnt exist, make it, else do nothing as to avoid duplicates.
     if (!myEle) {
       let content = `
-  <div class="card" id="${idx}">
-  <button class="delete-button" onclick="deleter(this)" id="${idx}"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg></button>
-    <div class="card-title">${result.title}</div>
-    <div class="card-author">${result.author}</div>
-              <div class="description">${result.description}</div>
-    <div class="read-status">
-      <button id="read-button">
-        ${result.read}
+        <div class="card" id="${idx}">
+          <button class="delete-button" onclick="deleter(this)" id="${idx}"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg></button>
+          <div class="card-title">${result.title}</div>
+          <div class="card-author">${result.author}</div>
+          <div class="description">${result.description}</div>
+          <div class="read-status">
+           <button class="read-button" id=${idx} onclick="changeReadStatus(this)">
+             ${result.read}
           </button>
         </div>
-        </div>
+      </div>
   `;
 
       cardContainer.innerHTML += content;
@@ -42,17 +50,17 @@ let run = () =>
 let run2 = () =>
   myLibrary.forEach((result, idx) => {
     let content = `
-  <div class="card" id="${idx}">
-  <button class="delete-button" onclick="deleter(this)" id="${idx}"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg></button>
-    <div class="card-title">${result.title}</div>
-    <div class="card-author">${result.author}</div>
-              <div class="description">${result.description}</div>
-    <div class="read-status">
-      <button id="read-button">
-        ${result.read}
+        <div class="card" id="${idx}">
+          <button class="delete-button" onclick="deleter(this)" id="${idx}"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg></button>
+          <div class="card-title">${result.title}</div>
+          <div class="card-author">${result.author}</div>
+          <div class="description">${result.description}</div>
+          <div class="read-status">
+           <button id="read-button" onclick="changeReadStatus(this)">
+             ${result.read}
           </button>
         </div>
-        </div>
+      </div>
   `;
 
     cardContainer.innerHTML += content;
@@ -65,14 +73,12 @@ let reset = () => {
 //buttons
 const addBookBtn = document.getElementById("addBookBtn");
 const submit = document.getElementById("submit");
-const deleteButton = document.querySelector(".delete-button");
 
 //containers
 const modal_container = document.getElementById("modal_container");
 const body = document.querySelector(".main");
 
 //form elements
-
 const form = document.querySelector(".form");
 const title_input = document.querySelector("#title");
 const author_input = document.querySelector("#author");
@@ -107,4 +113,10 @@ let deleter = (element) => {
   reset();
   myLibrary.splice(element.id, 1);
   run2();
+};
+
+let changeReadStatus = (element) => {
+  let r = myLibrary[element.id];
+  r.title = "yeah";
+  console.log(myLibrary[element.id]);
 };
